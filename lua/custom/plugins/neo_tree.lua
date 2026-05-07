a9ec2845806c9bb05acc02d58357139c8b320715
Nguyen, James <james.nguyen2+origin@originenergy.com.au>
2026-05-07 18:53:01 +1000

    fix: update neo_tree config to include the return

--- lua/custom/plugins/neo_tree.lua
+++ lua/custom/plugins/neo_tree.lua
@@ -1,9 +1,14 @@
-require("neo-tree").setup({
-  filesystem = {
-    filtered_items = {
-      visible = true,        -- show hidden items but dimmed
-      hide_dotfiles = false, -- show dotfiles
-      hide_gitignored = false, -- show gitignored files
-    },
-  },
-})
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,          -- show hidden items but dimmed
+          hide_dotfiles = false,   -- show dotfiles
+          hide_gitignored = false, -- show gitignored files
+        },
+      },
+    })
+  end,
+}
